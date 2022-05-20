fede94efd605cd2857ebb3d570bc329ce8b011ef
ismarsel <ismarsel@gmail.com>
2022-05-20 17:45:43 +0500

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
@@ -21,6 +15,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
